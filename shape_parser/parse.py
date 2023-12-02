8561aad1e67b67cf2a3f9f3dfa05bf7fed8a80e0
zpogrebin <zpogrebin111@gmail.com>
2023-12-02 17:39:47 -0500

    initial drawing complete

--- shape_parser/parse.py
+++ shape_parser/parse.py
@@ -8,18 +8,157 @@ Parses shapes from a shape SVG file and returns a list of shapes.
 
 import pathlib
 
-import svg
+import svgwrite
 import pandas as pd
 
 # CONSTANTS
 _SHAPE_FILE = pathlib.Path("./shape_parser/shapes.txt")
+_ROUTE_FILE = pathlib.Path("./shape_parser/shapes_by_route.csv")
 _OUTPUT_FILE = pathlib.Path("./shape_parser/shapes.svg")
+
+_BOSTON_ORIGIN = (42.3601, -71.0589)
+_LAT_LONG_SCALE = 1000
 
 class MapMaker:
 
-    def __init__(self, shape_file=_SHAPE_FILE):
-        self.load_shapes(shape_file)
+    shapes = {}
+    verbosity = 10
 
-    def load_shapes(self, shape_file=_SHAPE_FILE):
+    shape_file = None
+    route_file = None
+    force = None
+
+    map_bounds = None
+
+    def __init__(
+            self, shape_file=_SHAPE_FILE, route_file=_ROUTE_FILE,
+            force_reanalysis=True
+    ):
+        self.force = force_reanalysis
+        self.shape_file = shape_file
+        self.route_file = route_file
+        self.load_shapes()
+
+    def _print(self, message, level=1):
+        """Prints a message if the verbosity is greater than the level."""
+        if self.verbosity >= level:
+            print(message)
+
+    def load_shapes(self,):
+        """Loads shapes from a shape file and adds them to the shapes list."""
+        self._print("Loading shapes")
         self.shapes = []
-        
+        df = pd.read_csv(self.shape_file)
+        self.analyze(df)
+        for shape_id in df.shape_id.unique():
+            self._print(f"Loading shape {shape_id}", 3)
+            points = df[df.shape_id == shape_id]
+            self.shapes.append(points)
+
+    def analyze(self, df: pd.DataFrame, save=True):
+        """Analyzes the shapes dataframe."""
+        self.add_route_data(df, save=True)
+        self.convert_lat_long_to_points(df, save=True)
+        self.get_map_bounds(df)
+    
+    def get_map_bounds(self, df: pd.DataFrame):
+        self.map_bounds = []
+        absolute_max_x = abs(df.x).max()
+        absolute_max_y = abs(df.y).max()
+        self.map_bounds = [
+            -absolute_max_x, absolute_max_x, -absolute_max_y, absolute_max_y
+        ]
+    
+    def add_route_data(self, df: pd.DataFrame, save=True):
+        """Adds route data to the shapes dataframe based on the shape_id."""
+        self._print("Adding route data")
+        routes = pd.read_csv(self.route_file)
+        routes.columns = [i.lower() for i in routes.columns]
+        # Check if route columns are in the df, if so, return
+        if self._check_df_for_columns(df, routes):
+            return
+        # Re-index the dataframe
+        routes = routes.loc[~routes.shape_id.duplicated()]
+        # Remove shape_ids that are not in the routes dataframe
+        df = df[df.shape_id.isin(routes.shape_id)]
+        # Remove duplicate columns
+        routes = routes.set_index("shape_id")
+        columns = routes.columns
+        # Convert routes to a dictionary
+        routes = routes.to_dict(orient="index")
+        for column in columns:
+            self._print(f"Adding {column} to shapes dataframe", 2)
+            if column == "shape_id":
+                continue
+            df[column] = [
+                routes[shape_id][column] for shape_id in df.shape_id
+            ]
+        # Save the dataframe
+        if save:
+            df.to_csv(self.shape_file, index=False)
+
+    def convert_lat_long_to_points(self, df: pd.DataFrame, save=True):
+        """Converts lat and long to x and y coordinates."""
+        self._print("Converting lat and long to points")
+        # Check if the columns are already in the dataframe
+        if self._check_df_for_columns(df, pd.DataFrame({"x": [], "y": []})):
+            return
+        # Convert lat and long to x and y
+        df["x"], df["y"] = zip(*df.apply(
+            lambda row: self._lat_long_to_xy(
+                row["shape_pt_lat"], 
+                row["shape_pt_lon"]),
+            axis=1
+        ))
+        # Save the dataframe
+        if save:
+            df.to_csv(self.shape_file, index=False)
+
+    def _check_df_for_columns(self, df: pd.DataFrame, new: pd.DataFrame):
+        """Checks if the new_df columns are in the df."""
+        if self.force:
+            return False
+        if isinstance(new, pd.DataFrame):
+            new = new.columns
+        if all([i in df.columns for i in new]):
+            self._print("Data already in dataframe :)", 2)
+            return True
+        return False
+
+    def _lat_long_to_xy(self, lat, long):
+        """Converts lat and long to x and y coordinates."""
+        x = (long - _BOSTON_ORIGIN[1]) * _LAT_LONG_SCALE
+        y = - (lat - _BOSTON_ORIGIN[0]) * _LAT_LONG_SCALE
+        return x, y
+    
+    def make_svg(self, output_file=_OUTPUT_FILE):
+        """Makes an SVG file from the shapes."""
+        self._print("Making SVG")
+        svgwrite.AUTHOR_EMAIL = "zpogrebin111@gmail.com"
+        svgwrite.AUTHOR_NAME = "Zev Pogrebin"
+        drawing: svgwrite.Drawing = svgwrite.Drawing(
+            str(output_file), 
+            size=(
+                f"{self.map_bounds[1] - self.map_bounds[0]}px", 
+                f"{self.map_bounds[3] - self.map_bounds[2]}px"
+            )
+        )
+        for shape in self.shapes:
+            self._print(f"Drawing shape {shape.shape_id.iloc[0]}", 3)
+            points = shape[["x", "y"]].values.tolist()
+            # Shift points by the minimum x and y values
+            points = [
+                (point[0] - self.map_bounds[0], point[1] - self.map_bounds[2])
+                for point in points
+            ]
+            drawing.add(drawing.polyline(
+                points, 
+                stroke="#" + shape.route_color.iloc[0], 
+                fill="none")
+            )
+        drawing.save()
+
+if __name__ == "__main__":
+    maker = MapMaker()
+    maker.make_svg()
+    print(maker.shapes)
